8cbcef4644cd2f91cf984bbdbb9b4d9c4965d256
cr3314 <75927147+cr3314@users.noreply.github.com>
2025-09-05 11:19:40 +0000

    Dedupe a bit

--- src/webm-alpha.ts
+++ src/webm-alpha.ts
@@ -18,22 +18,27 @@ import { isSafari, promiseWithResolvers } from './misc';
 import { EncodedPacket } from './packet';
 import { VideoSample } from './sample';
 
-class WebMSeparateAlphaDecoder extends CustomVideoDecoder {
-	private mainCoder!: VideoDecoder;
-	private alphaCoder!: VideoDecoder;
+type CoderType = VideoDecoder | VideoEncoder;
+type CoderOutput<T extends CoderType> = T extends VideoDecoder
+	? VideoFrame
+	: EncodedVideoChunk;
 
-	private mainCoderError: Error | null = null;
-	private alphaCoderError: Error | null = null;
+class Coder<T extends CoderType> {
+	mainCoder!: T;
+	alphaCoder!: T;
+
+	mainCoderError: Error | null = null;
+	alphaCoderError: Error | null = null;
 
 	// Main output needs to wait for alpha output to be ready, or null due to unexpected issue
-	private alphaOutputResolverMap = new Map<
+	alphaOutputResolverMap = new Map<
 		number,
-		ReturnType<typeof promiseWithResolvers<VideoFrame | null>>
+		ReturnType<typeof promiseWithResolvers<CoderOutput<T> | null>>
 	>();
 
-	private closed = false;
+	closed = false;
 
-	private setAlphaCoderError(error: Error) {
+	setAlphaCoderError(error: Error) {
 		if (this.alphaCoderError) {
 			return;
 		}
@@ -44,75 +49,6 @@ class WebMSeparateAlphaDecoder extends CustomVideoDecoder {
 
 		this.alphaOutputResolverMap.clear();
 		this.alphaCoderError = error;
-	}
-
-	// Should check if input actually has alpha instead of by 'prefer-software', but no access to _backing here
-	static override supports(codec: VideoCodec, config: VideoDecoderConfig) {
-		// Hardware accelerated have impractically worse performance in `copyTo` based implementation of _combineAlpha
-		return ['vp9', 'av1', 'vp8'].includes(codec)
-			&& config.hardwareAcceleration === 'prefer-software'
-			&& typeof VideoDecoder !== 'undefined'
-			&& typeof VideoFrame !== 'undefined'
-			&& !isSafari(); // Safari v18.6: broken when creating I420A, I420 works with the same data buffer
-	}
-
-	override init() {
-		this.mainCoder = new VideoDecoder({
-			output: (output) => {
-				const resolver = this._getAlphaOutputResolver(output.timestamp);
-				(async () => {
-					let alpha;
-					let frame;
-
-					try {
-						alpha = await resolver.promise.catch(() => {});
-						frame = await this._combineAlpha(output, alpha);
-
-						this.onSample(new VideoSample(frame));
-					} finally {
-						if (frame !== output) output.close();
-						alpha?.close();
-					}
-				})().catch(() => {});
-			},
-
-			error: (error) => {
-				this.mainCoderError = error;
-				this.close();
-			},
-		});
-		this.mainCoder.configure(this.config);
-		this.alphaCoder = new VideoDecoder({
-			output: (output) => {
-				if (this.mainCoderError || this.alphaCoderError || this.closed) {
-					return output.close();
-				}
-
-				this._getAlphaOutputResolver(output.timestamp).resolve(output);
-			},
-			error: error => this.setAlphaCoderError(error),
-		});
-		this.alphaCoder.configure(this.config);
-	}
-
-	override decode(packet: EncodedPacket) {
-		const resolver = this._getAlphaOutputResolver(packet.microsecondTimestamp);
-
-		if (packet.additions) {
-			this.alphaCoder.decode(
-				new EncodedPacket(
-					packet.additions,
-					packet.type,
-					packet.timestamp,
-					packet.duration,
-					packet.sequenceNumber,
-				).toEncodedVideoChunk(),
-			);
-		} else {
-			resolver.resolve(null);
-		}
-
-		this.mainCoder.decode(packet.toEncodedVideoChunk());
 	}
 
 	async flush() {
@@ -130,15 +66,14 @@ class WebMSeparateAlphaDecoder extends CustomVideoDecoder {
 		await Promise.allSettled(promises);
 	}
 
-	override close() {
+	close() {
 		if (this.mainCoder.state !== 'closed') this.mainCoder.close();
 		if (this.alphaCoder.state !== 'closed') this.alphaCoder.close();
 		this.closed = true;
 		this.alphaOutputResolverMap.clear();
 	}
 
-	/** @internal */
-	_getAlphaOutputResolver(microsecondTimestamp: number) {
+	getAlphaOutputResolver(microsecondTimestamp: number) {
 		if (!this.alphaOutputResolverMap.has(microsecondTimestamp)) {
 			this.alphaOutputResolverMap.set(microsecondTimestamp, promiseWithResolvers());
 		}
@@ -152,69 +87,141 @@ class WebMSeparateAlphaDecoder extends CustomVideoDecoder {
 		return resolver;
 	}
 
-	/** @internal */
-	async _combineAlpha(main: VideoFrame, alpha?: VideoFrame | null | void) {
-		// No alpha, or closed
-		if (!alpha || alpha.displayWidth === 0) {
-			return main;
-		}
+	shouldIgnoreAlphaOutput() {
+		return this.mainCoderError || this.alphaCoderError || this.closed;
+	}
+}
 
-		const format = main.format || '';
-		const isYUV = format.startsWith('I');
-		const resultFormat = isYUV
-			? `${format.slice(0, 4)}A${format.slice(4, format.length)}` as VideoPixelFormat // e.g. I420AP12
-			: format.endsWith('X')
-				? `${format.slice(0, 3)}A` as VideoPixelFormat
-				: format;
+const combineAlpha = async (main: VideoFrame, alpha?: VideoFrame | null | void) => {
+	// No alpha, or closed
+	if (!alpha || alpha.displayWidth === 0) {
+		return main;
+	}
 
-		// Already has A, or NV12 (mainly from hardware accelerated cases)
-		if (!format || resultFormat === format) {
-			this.setAlphaCoderError(new Error(`Unsupported format: ${format}`));
+	const format = main.format || '';
+	const isYUV = format.startsWith('I');
+	const resultFormat = isYUV
+		? `${format.slice(0, 4)}A${format.slice(4, format.length)}` as VideoPixelFormat // e.g. I420AP12
+		: format.endsWith('X')
+			? `${format.slice(0, 3)}A` as VideoPixelFormat
+			: format;
 
-			return main;
-		};
+	// Already has A, or NV12 (mainly from hardware accelerated cases)
+	if (!format || resultFormat === format) {
+		return main;
+	};
 
-		try {
-			const mainSize = main.allocationSize();
-			const alphaSize = alpha.allocationSize();
-			const data = new Uint8Array(mainSize + alphaSize);
-			const alphaArray = data.subarray(mainSize, data.byteLength);
+	const mainSize = main.allocationSize();
+	const alphaSize = alpha.allocationSize();
+	const data = new Uint8Array(mainSize + alphaSize);
+	const alphaArray = data.subarray(mainSize, data.byteLength);
 
-			await Promise.all([
-				main.copyTo(data.subarray(0, mainSize)),
-				alpha.copyTo(alphaArray),
-			]);
+	await Promise.all([
+		main.copyTo(data.subarray(0, mainSize)),
+		alpha.copyTo(alphaArray),
+	]);
 
-			// YUV planar format are ready as-is.
-			// But for RGB formats, need to copy 1 channel to X channel.
-			// For alpha channel, UV are dummy
-			// Optimization: most implementation should give the same RGB values by filling UV with max value
-			// Otherwise need luminanceToAlpha, ref: https://www.w3.org/TR/SVG11/filters.html#feColorMatrixElement
-			// e.g. Chrome: I420, Firefox: BGRX.
-			if (!isYUV) {
-				for (let i = 0; i < alphaSize; i += 4) {
-					data[i + 3] = data[mainSize + i]!;
-				}
-			}
-
-			return new VideoFrame(data, {
-				codedWidth: main.displayWidth, // As a result of copyTo
-				codedHeight: main.displayHeight,
-				displayWidth: main.displayWidth,
-				displayHeight: main.displayHeight,
-				colorSpace: main.colorSpace,
-				timestamp: main.timestamp,
-				duration: main.duration ?? undefined,
-				format: resultFormat,
-				transfer: [data.buffer],
-			} as VideoFrameBufferInit);
-		} catch (error) {
-			/** @ts-expect-error ECMAScript target */
-			this.setAlphaCoderError(new Error('Combine alpha failed', { cause: error }));
-
-			return main;
+	// YUV planar format are ready as-is.
+	// But for RGB formats, need to copy 1 channel to X channel.
+	// For alpha channel, UV are dummy
+	// Optimization: most implementation should give the same RGB values by filling UV with max value
+	// Otherwise need luminanceToAlpha, ref: https://www.w3.org/TR/SVG11/filters.html#feColorMatrixElement
+	// e.g. Chrome: I420, Firefox: BGRX.
+	if (!isYUV) {
+		for (let i = 0; i < alphaSize; i += 4) {
+			data[i + 3] = data[mainSize + i]!;
 		}
 	}
+
+	return new VideoFrame(data, {
+		codedWidth: main.displayWidth, // As a result of copyTo
+		codedHeight: main.displayHeight,
+		displayWidth: main.displayWidth,
+		displayHeight: main.displayHeight,
+		colorSpace: main.colorSpace,
+		timestamp: main.timestamp,
+		duration: main.duration ?? undefined,
+		format: resultFormat,
+		transfer: [data.buffer],
+	} as VideoFrameBufferInit);
+};
+
+class WebMSeparateAlphaDecoder extends CustomVideoDecoder {
+	coder = new Coder<VideoDecoder>();
+
+	// Should check if input actually has alpha instead of by 'prefer-software', but no access to _backing here
+	static override supports(codec: VideoCodec, config: VideoDecoderConfig) {
+		// Hardware accelerated have impractically worse performance in `copyTo` based implementation of _combineAlpha
+		return ['vp9', 'av1', 'vp8'].includes(codec)
+			&& config.hardwareAcceleration === 'prefer-software'
+			&& typeof VideoDecoder !== 'undefined'
+			&& typeof VideoFrame !== 'undefined'
+			&& !isSafari(); // Safari v18.6: broken when creating I420A, I420 works with the same data buffer
+	}
+
+	override init() {
+		this.coder.mainCoder = new VideoDecoder({
+			output: (output) => {
+				const resolver = this.coder.getAlphaOutputResolver(output.timestamp);
+
+				(async () => {
+					let alpha;
+					let frame;
+
+					try {
+						alpha = await resolver.promise.catch(() => {});
+						frame = await combineAlpha(output, alpha).catch(() => {
+							this.coder.setAlphaCoderError(new Error('Failed to combine alpha'));
+							return output;
+						});
+						this.onSample(new VideoSample(frame));
+					} finally {
+						if (frame !== output) output.close();
+						alpha?.close();
+					}
+				})().catch(() => {});
+			},
+			error: (error) => {
+				this.coder.mainCoderError = error;
+				this.close();
+			},
+		});
+		this.coder.mainCoder.configure(this.config);
+		this.coder.alphaCoder = new VideoDecoder({
+			output: (output) => {
+				if (this.coder.shouldIgnoreAlphaOutput()) {
+					return output.close();
+				}
+
+				this.coder.getAlphaOutputResolver(output.timestamp).resolve(output);
+			},
+			error: error => this.coder.setAlphaCoderError(error),
+		});
+		this.coder.alphaCoder.configure(this.config);
+	}
+
+	override decode(packet: EncodedPacket) {
+		const resolver = this.coder.getAlphaOutputResolver(packet.microsecondTimestamp);
+
+		if (packet.additions) {
+			this.coder.alphaCoder.decode(
+				new EncodedPacket(
+					packet.additions,
+					packet.type,
+					packet.timestamp,
+					packet.duration,
+					packet.sequenceNumber,
+				).toEncodedVideoChunk(),
+			);
+		} else {
+			resolver.resolve(null);
+		}
+
+		this.coder.mainCoder.decode(packet.toEncodedVideoChunk());
+	}
+
+	override close = () => this.coder.close();
+	override flush = () => this.coder.flush();
 }
 
 /**
@@ -239,33 +246,50 @@ export const registerWebMSeparateAlphaDecoder = () => registerDecoder(WebMSepara
 /** @internal */
 export const isWebMSeparateAlphaDecoderRegistered = () => customVideoDecoders.includes(WebMSeparateAlphaDecoder);
 
-class WebMSeparateAlphaEncoder extends CustomVideoEncoder {
-	private mainCoder!: VideoEncoder;
-	private alphaCoder!: VideoEncoder;
+const extractAlpha = async (videoSample: VideoSample) => {
+	const format = videoSample.format || '';
 
-	private mainCoderError: Error | null = null;
-	private alphaCoderError: Error | null = null;
+	if (
+		!format.includes('A') // Chrome v139: HEVC with alpha might give null, but cannot copyTo anyway
+		|| videoSample._closed
+		|| format.length > 5 // Not handling cases like I444AP12, not possible in browser now, will become null
+	) {
+		return null;
+	};
 
-	// Main output needs to wait for alpha output to be ready, or null due to unexpected issue
-	private alphaOutputResolverMap = new Map<
-		number,
-		ReturnType<typeof promiseWithResolvers<EncodedVideoChunk | null>>
-	>();
+	// RGB formats (like canvas) will get converted to 16-255 in browsers, and give broken result
+	const isYUV = format.startsWith('I') || format.startsWith('N');
+	const finalFormat = isYUV ? format.slice(0, 4) : 'I420';
+	const size = videoSample.allocationSize();
+	const data = new Uint8Array(size);
+	const pixels = videoSample.codedWidth * videoSample.codedHeight;
 
-	private closed = false;
+	await videoSample.copyTo(data);
 
-	private setAlphaCoderError(error: Error) {
-		if (this.alphaCoderError) {
-			return;
+	if (isYUV) {
+		data.set(data.subarray(size - pixels, size));
+	} else {
+		for (let i = 0; i < pixels; i++) {
+			data[i] = data[i * 4 + 3]!;
 		}
-
-		for (const resolver of this.alphaOutputResolverMap.values()) {
-			resolver.resolve(null);
-		}
-
-		this.alphaOutputResolverMap.clear();
-		this.alphaCoderError = error;
 	}
+
+	// eslint-disable-next-line @stylistic/max-len
+	// More expensive to encode otherwise, according to https://source.chromium.org/chromium/chromium/src/+/main:media/video/alpha_video_encoder_wrapper.cc;l=117
+	data.fill(255, pixels, data.byteLength);
+
+	return new VideoFrame(data, {
+		timestamp: videoSample.microsecondTimestamp,
+		duration: videoSample.microsecondDuration,
+		format: finalFormat,
+		codedWidth: videoSample.codedWidth,
+		codedHeight: videoSample.codedHeight,
+		transfer: [data.buffer],
+	} as VideoFrameBufferInit);
+};
+
+class WebMSeparateAlphaEncoder extends CustomVideoEncoder {
+	coder = new Coder<VideoEncoder>();
 
 	// Should check if output format supports also, but no access in CustomVideoEncoder
 	static override supports(codec: VideoCodec, config: VideoEncoderConfig) {
@@ -282,9 +306,9 @@ class WebMSeparateAlphaEncoder extends CustomVideoEncoder {
 			alpha: 'discard',
 		};
 
-		this.mainCoder = new VideoEncoder({
+		this.coder.mainCoder = new VideoEncoder({
 			output: (chunk, metadata) => {
-				const resolver = this._getAlphaOutputResolver(chunk.timestamp);
+				const resolver = this.coder.getAlphaOutputResolver(chunk.timestamp);
 				const packet = EncodedPacket.fromEncodedChunk(chunk);
 
 				(async () => {
@@ -300,128 +324,52 @@ class WebMSeparateAlphaEncoder extends CustomVideoEncoder {
 				})().catch(() => {});
 			},
 			error: (error) => {
-				this.mainCoderError = error;
+				this.coder.mainCoderError = error;
 				this.close();
 			},
 		});
-		this.mainCoder.configure(config);
+		this.coder.mainCoder.configure(config);
 
 		// Better add when there is really data and properly check configure in supports first
 		// Need refactoring of current API
-		this.alphaCoder = new VideoEncoder({
+		this.coder.alphaCoder = new VideoEncoder({
 			output: (chunk) => {
-				if (this.mainCoderError || this.alphaCoderError || this.closed) {
+				if (this.coder.shouldIgnoreAlphaOutput()) {
 					return;
 				}
-
-				this._getAlphaOutputResolver(chunk.timestamp).resolve(chunk);
+				this.coder.getAlphaOutputResolver(chunk.timestamp).resolve(chunk);
 			},
-			error: error => this.setAlphaCoderError(error),
+			error: error => this.coder.setAlphaCoderError(error),
 		});
-		this.alphaCoder.configure(config);
+		this.coder.alphaCoder.configure(config);
 	}
 
 	override async encode(videoSample: VideoSample, options: VideoEncoderEncodeOptions) {
-		if (this.mainCoderError) {
+		if (this.coder.mainCoderError) {
 			/** @ts-expect-error ECMAScript target */
-			throw new Error('Cannot encode on a closed VideoEncoder', { cause: this.mainCoderError });
+			throw new Error('Cannot encode on a closed VideoEncoder', { cause: this.coder.mainCoderError });
 		}
 
-		const alpha = await this._extractAlpha(videoSample);
+		const resolver = this.coder.getAlphaOutputResolver(videoSample.microsecondTimestamp);
+		const alpha = await extractAlpha(videoSample).catch(() => {
+			this.coder.setAlphaCoderError(new Error('Failed to extractAlpha'));
+		});
 
 		if (alpha) {
-			this.alphaCoder.encode(alpha, options);
+			this.coder.alphaCoder.encode(alpha, options);
 			alpha.close();
 		} else {
-			this._getAlphaOutputResolver(videoSample.microsecondTimestamp).resolve(null);
+			resolver.resolve(null);
 		}
 
 		const frame = videoSample.toVideoFrame();
 
-		this.mainCoder.encode(frame, options);
+		this.coder.mainCoder.encode(frame, options);
 		frame.close();
 	}
 
-	async flush() {
-		await Promise.all([
-			this.mainCoder.flush(),
-			this.alphaCoder.flush(),
-		]);
-
-		const promises = [];
-
-		for (const resolver of this.alphaOutputResolverMap.values()) {
-			promises.push(resolver.promise);
-		}
-
-		await Promise.allSettled(promises);
-	}
-
-	override close() {
-		if (this.mainCoder.state !== 'closed') this.mainCoder.close();
-		if (this.alphaCoder.state !== 'closed') this.alphaCoder.close();
-		this.closed = true;
-		this.alphaOutputResolverMap.clear();
-	}
-
-	/** @internal */
-	_getAlphaOutputResolver(microsecondTimestamp: number) {
-		if (!this.alphaOutputResolverMap.has(microsecondTimestamp)) {
-			this.alphaOutputResolverMap.set(microsecondTimestamp, promiseWithResolvers());
-		}
-
-		const resolver = this.alphaOutputResolverMap.get(microsecondTimestamp)!;
-
-		if (this.mainCoderError || this.alphaCoderError || this.closed) {
-			resolver.resolve(null);
-		}
-
-		return resolver;
-	}
-
-	/** @internal */
-	async _extractAlpha(videoSample: VideoSample) {
-		const format = videoSample.format || '';
-
-		if (
-			!format.includes('A') // Chrome v139: HEVC with alpha might give null, but cannot copyTo anyway
-			|| videoSample._closed
-			|| this.alphaCoderError
-			|| format.length > 5 // Not handling cases like I444AP12, not possible in browser now, will become null
-		) {
-			return null;
-		};
-
-		// RGB formats (like canvas) will get converted to 16-255 in browsers, and give broken result
-		const isYUV = format.startsWith('I') || format.startsWith('N');
-		const finalFormat = isYUV ? format.slice(0, 4) : 'I420';
-		const size = videoSample.allocationSize();
-		const data = new Uint8Array(size);
-		const pixels = videoSample.codedWidth * videoSample.codedHeight;
-
-		await videoSample.copyTo(data);
-
-		if (isYUV) {
-			data.set(data.subarray(size - pixels, size));
-		} else {
-			for (let i = 0; i < pixels; i++) {
-				data[i] = data[i * 4 + 3]!;
-			}
-		}
-
-		// eslint-disable-next-line @stylistic/max-len
-		// More expensive to encode otherwise, according to https://source.chromium.org/chromium/chromium/src/+/main:media/video/alpha_video_encoder_wrapper.cc;l=117
-		data.fill(255, pixels, data.byteLength);
-
-		return new VideoFrame(data, {
-			timestamp: videoSample.microsecondTimestamp,
-			duration: videoSample.microsecondDuration,
-			format: finalFormat,
-			codedWidth: videoSample.codedWidth,
-			codedHeight: videoSample.codedHeight,
-			transfer: [data.buffer],
-		} as VideoFrameBufferInit);
-	}
+	override close = () => this.coder.close();
+	override flush = () => this.coder.flush();
 }
 
 /**
